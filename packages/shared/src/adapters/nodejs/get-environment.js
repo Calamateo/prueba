440968463d9eed6096cfbc35810927f21e2860d0
Daniel Calamateo <dcalamateo@bapcorporativo.com>
2023-07-12 15:37:23 -0600

    feat(nodejs): se agrego els delete plantel

--- packages/shared/src/adapters/nodejs/get-environment.js
+++ packages/shared/src/adapters/nodejs/get-environment.js
@@ -6,13 +6,15 @@ const {
   isDevEnvironment,
   isTestEnvironment,
 } = require('../../utils/checkers');
+const isCiEnvironment = require('../../utils/checkers/is-ci');
 
 function getEnvironment() {
   if (isUndefined(nodeEnv)) {
     throw ReferenceError('reference to undefined property "NODE_ENV"');
   } else if (!isProdEnvironment(nodeEnv)
   && !isDevEnvironment(nodeEnv)
-  && !isTestEnvironment(nodeEnv)) {
+  && !isTestEnvironment(nodeEnv)
+  && !isCiEnvironment(nodeEnv)) {
     throw new TypeError('Invalid assignment to const "NODE_ENV"');
   }
 
